8ca07971a1183779aea5ec1d67d99056b3dc145a
Kev9794 <k.natanael88@gmail.com>
2022-10-20 20:23:08 +0700

    Homework

--- app/src/main/java/com/example/android_room/NewWordActivity.java
+++ app/src/main/java/com/example/android_room/NewWordActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_room;
 
+import static com.example.android_room.MainActivity.EXTRA_DATA_UPDATE_WORD;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,18 +9,33 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import static com.example.android_room.MainActivity.EXTRA_DATA_ID;
+import static com.example.android_room.MainActivity.EXTRA_DATA_UPDATE_WORD;
 
 public class NewWordActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY =
             "com.example.android.roomwordssample.REPLY";
 
     private EditText mEditWordView;
+    public static final String EXTRA_REPLY_ID = "com.example.android_room.roomwordssample.REPLY_ID";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
         mEditWordView = findViewById(R.id.edit_word);
+        int id = -1 ;
+
+        final Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String word = extras.getString(EXTRA_DATA_UPDATE_WORD, "");
+            if (!word.isEmpty()) {
+                mEditWordView.setText(word);
+                mEditWordView.setSelection(word.length());
+                mEditWordView.requestFocus();
+            }
+        }
 
         final Button button = findViewById(R.id.button_save);
         button.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +46,12 @@ public class NewWordActivity extends AppCompatActivity {
                 } else {
                     String word = mEditWordView.getText().toString();
                     replyIntent.putExtra(EXTRA_REPLY, word);
+                    if (extras != null && extras.containsKey(EXTRA_DATA_ID)) {
+                        int id = extras.getInt(EXTRA_DATA_ID, -1);
+                        if (id != -1) {
+                            replyIntent.putExtra(EXTRA_REPLY_ID, id);
+                        }
+                    }
                     setResult(RESULT_OK, replyIntent);
                 }
                 finish();
